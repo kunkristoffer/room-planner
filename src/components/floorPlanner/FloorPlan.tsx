f004dc400a575d3aab0e9dd03db78d8f8f49891a
Kristoffer <kunkristoffer@gmail.com>
2026-04-24 10:10:08 +0200

    Added temp debug sliders for translate and rotation

--- src/components/floorPlanner/FloorPlan.tsx
+++ src/components/floorPlanner/FloorPlan.tsx
@@ -1,5 +1,6 @@
 import { Floor1 } from "@/data/loadFloors";
 import { FloorNumberSlider } from "../ui/sliders/FloorNumber";
+import { useState } from "react";
 
 interface FloorPlanProps {
   floor: number;
@@ -7,11 +8,43 @@ interface FloorPlanProps {
   handleClick: (id: string, type: "floor" | "room") => void;
 }
 
+/** Notes:
+ * Needs two views: top-down and angled, needs to be stored in state
+ */
+
 export function FloorPlan({ floor, handleClick }: FloorPlanProps) {
-  const floors = Array.from({ length: 5 }, (_, i) => i);
+  const floors = Array.from({ length: 10 }, (_, i) => i);
+
+  // Transform svgs
+  const [transformX, setTransformX] = useState(0);
+  const [transformY, setTransformY] = useState(250);
+  const [transformZ, setTransformZ] = useState(250);
+
+  // Transform container
+  const [rotateX, setRotateX] = useState(70);
+  const [rotateY, setRotateY] = useState(0);
+  const [rotateZ, setRotateZ] = useState(25);
 
   function offset(i: number) {
     return i - floor;
+  }
+
+  function getOpacity(
+    index: number,
+    selectedIndex: number,
+    maxDistance: number,
+  ) {
+    const distance = Math.abs(index - selectedIndex);
+
+    const minOpacity = 0;
+    const maxOpacity = 0.9;
+
+    const normalized = Math.min(distance / maxDistance, 1);
+
+    // Ease-out curve (fast increase, slow towards max)
+    const eased = 1 - Math.pow(normalized, 2);
+
+    return minOpacity + (maxOpacity - minOpacity) * eased;
   }
 
   return (
@@ -19,19 +52,22 @@ export function FloorPlan({ floor, handleClick }: FloorPlanProps) {
       <div className="perspective-distantes flex-1">
         <div
           className="relative transform-3d h-[50svw]"
-          style={{ transform: "rotateX(70deg) rotateZ(25deg)" }}
+          style={{
+            transform: `rotateX(${rotateX}deg) rotateY(${rotateY}deg) rotateZ(${rotateZ}deg)`,
+          }}
         >
           {floors.map((_, i) => (
             <Floor1
               key={i}
               data-floor={i}
-              className={`absolute ${floor === i ? "bg-white" : ""}`}
+              className={`absolute ${floor === i ? "" : ""}`}
               style={{
                 transform: `
-                  translateZ(${offset(i) * 100}px)
-                  translateY(-${Math.abs(offset(i)) * 150}px)
+                  translateX(${offset(i) * transformX}px)
+                  translateY(${offset(i) * transformY}px)
+                  translateZ(${offset(i) * transformZ}px)
                 `,
-                opacity: 1 - Math.abs(offset(i)) / floors.length,
+                opacity: getOpacity(i, floor, floors.length),
                 // zIndex: 100 - Math.abs(offset(i)),
               }}
             />
@@ -61,6 +97,68 @@ export function FloorPlan({ floor, handleClick }: FloorPlanProps) {
             />
           ))} */}
         </div>
+      </div>
+      <div className="text-black z-50">
+        <label>
+          <p>Transform X - {transformX}</p>
+          <input
+            type="range"
+            value={transformX}
+            min={0}
+            max={500}
+            onChange={(e) => setTransformX(e.currentTarget.valueAsNumber)}
+          />
+        </label>
+        <label>
+          <p>Transform Y - {transformY}</p>
+          <input
+            type="range"
+            value={transformY}
+            min={0}
+            max={500}
+            onChange={(e) => setTransformY(e.currentTarget.valueAsNumber)}
+          />
+        </label>
+        <label>
+          <p>Transform Z - {transformZ}</p>
+          <input
+            type="range"
+            value={transformZ}
+            min={0}
+            max={500}
+            onChange={(e) => setTransformZ(e.currentTarget.valueAsNumber)}
+          />
+        </label>
+        <label>
+          <p>Rotate X - {rotateX}</p>
+          <input
+            type="range"
+            value={rotateX}
+            min={-360}
+            max={360}
+            onChange={(e) => setRotateX(e.currentTarget.valueAsNumber)}
+          />
+        </label>
+        <label>
+          <p>Rotate Y - {rotateY}</p>
+          <input
+            type="range"
+            value={rotateY}
+            min={-360}
+            max={360}
+            onChange={(e) => setRotateY(e.currentTarget.valueAsNumber)}
+          />
+        </label>
+        <label>
+          <p>Rotate Z - {rotateZ}</p>
+          <input
+            type="range"
+            value={rotateZ}
+            min={-360}
+            max={360}
+            onChange={(e) => setRotateZ(e.currentTarget.valueAsNumber)}
+          />
+        </label>
       </div>
       <FloorNumberSlider
         floor={floor}
