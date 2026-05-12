f228a840f700c9f93e343cad33ecc0c58ef8c087
Kristoffer <kunkristoffer@gmail.com>
2026-05-12 11:32:40 +0200

    Added function for handling mouse move and getting relative position of pointer

--- src/components/ui/inputs/FloorSlider.tsx
+++ src/components/ui/inputs/FloorSlider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ViewMode } from "@/utils/styles";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 interface FloorSliderProps {
   curFloor: number;
@@ -25,21 +25,50 @@ export function FloorSlider({
   const maxFloor = Math.max(...floors);
   const floorRange = floors.length;
 
-  console.log(availableFloors, floors);
-
-  function getTrackPosition(floor: number) {
-    return 10;
+  function getTrackPosition(floor: number, range: number) {
+    return (range / 100) * floor;
   }
 
-  function getClosestAvailableFloor() {}
+  function getClosestAvailableFloor(floor: number, avilable: number[]) {}
+
+  useEffect(() => {
+    if (!isDraging) return;
+
+    function handleMove(e: PointerEvent) {
+      if (!trackRef.current) return;
+      const containerSize = trackRef.current.getBoundingClientRect();
+      const relativeHeight = e.clientY - containerSize.top;
+      const relativeToPercent = Math.min(
+        Math.max((relativeHeight / containerSize.height) * 100, 0),
+        100,
+      );
+      console.log(relativeToPercent);
+    }
+
+    function handleRelease() {
+      setIsDraging(false);
+    }
+
+    window.addEventListener("pointermove", handleMove);
+    window.addEventListener("pointerup", handleRelease);
+
+    return () => {
+      window.removeEventListener("pointermove", handleMove);
+      window.removeEventListener("pointerup", handleRelease);
+    };
+  }, [isDraging]);
 
   return (
     <div ref={trackRef} className="relative flex h-48 cursor-pointer">
+      <p className="absolute left-24 text-black text-nowrap bg-white p-2 border">
+        is draging: {isDraging ? "true" : "false"}
+      </p>
       <span className="h-full w-3 rounded-md bg-gray-300 border border-slate-500"></span>
       <button
         type="button"
-        className="absolute left-1/2 -translate-x-1/2 size-6 bg-blue-600 rounded-full"
-        style={{ top: `${getTrackPosition(curFloor)}%` }}
+        className="absolute left-1/2 -translate-x-1/2 size-6 bg-blue-600 rounded-full active:bg-blue-500"
+        style={{ top: `${getTrackPosition(curFloor, floorRange)}%` }}
+        onPointerDown={() => setIsDraging(true)}
       ></button>
     </div>
   );
